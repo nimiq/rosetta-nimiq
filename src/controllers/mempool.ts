851174b83a5770264e33616655de610c84b9b165
Sören <hello@soerenschwert.de>
2021-12-16 12:12:20 +0100

    Implement data:mempool endpoints

---
+++ src/controllers/mempool.ts
@@ -0,0 +1,39 @@
+import { Router } from "https://deno.land/x/oak/mod.ts";
+import rpc from "../rpc.ts"
+import {
+    validateNetwork,
+} from '../lib/validation.ts'
+import {
+    rpcTransationToRosetta,
+} from '../lib/conversion.ts'
+
+import type { Paths } from '../../types/rosetta.d.ts'
+import type { NimiqRpc } from '../../types/nimiq_rpc.d.ts'
+
+export default new Router()
+    .post("/", async ({ request, response }) => {
+        const req = await request.body().value as Paths.Mempool.RequestBody
+
+        validateNetwork(req.network_identifier)
+
+        const hashes = await rpc<string[]>('mempoolContent')
+
+        const result: Paths.Mempool.Responses.$200 = {
+            transaction_identifiers: hashes.map(hash => ({ hash })),
+        }
+
+        response.body = result
+    })
+    .post("/transaction", async ({ request, response }) => {
+        const req = await request.body().value as Paths.MempoolTransaction.RequestBody
+
+        validateNetwork(req.network_identifier)
+
+        const transaction = await rpc<NimiqRpc.Transaction>('getTransactionByHash', req.transaction_identifier.hash)
+
+        const result: Paths.MempoolTransaction.Responses.$200 = {
+            transaction: rpcTransationToRosetta(transaction),
+        }
+
+        response.body = result
+    })
--- src/mod.ts
+++ src/mod.ts
@@ -3,11 +3,13 @@ import { Application, Router } from "https://deno.land/x/oak/mod.ts";
 import network from './controllers/network.ts'
 import block from './controllers/block.ts'
 import account from './controllers/account.ts'
+import mempool from './controllers/mempool.ts'
 
 const root = new Router()
     .use("/network", network.routes(), network.allowedMethods())
     .use("/block", block.routes(), block.allowedMethods())
     .use("/account", account.routes(), account.allowedMethods())
+    .use("/mempool", mempool.routes(), mempool.allowedMethods())
 
 console.log("Rosetta server running. Access it at http://localhost:8080/")
 await new Application()
