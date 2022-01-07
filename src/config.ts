4eca95ce201e290a1a0b7923cf2ef1f4c8f629e0
Sören <hello@soerenschwert.de>
2022-01-07 10:06:02 +0100

    Accept an external config file

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,38 @@
-export default {
-    network: "devnet",
+import defaultConfig from "./config.default.json" assert { type: "json" };
+
+type Config = {
+    network: "mainnet" | "testnet" | "devnet",
     rpc: {
-        endpoint: "http://localhost",
-        port: 8648,
+        endpoint: string,
+        port: number,
     },
-} as const;
+}
+
+let userConfig;
+try {
+    userConfig = Deno.args.length ? JSON.parse(Deno.readTextFileSync(Deno.args[0])) : {};
+} catch (error) {
+    console.error('Failed to parse provided config file:');
+    throw error;
+}
+
+// Testing for object type is fun, because everything is an object in Javascript, even `null`
+if (typeof userConfig !== 'object' || !userConfig || Array.isArray(userConfig)) {
+    throw new Error('Invalid provided config file: not a JSON object');
+}
+
+// Validate that userConfig only includes known options
+for (const option in userConfig) {
+    if (!(option in defaultConfig)) {
+        throw new Error(`Unknown option '${option}' in provided config file`);
+    }
+}
+
+const config: Config = {
+    ...defaultConfig,
+    ...userConfig,
+};
+
+console.debug("Config:", config);
+
+export default Object.freeze(config);
