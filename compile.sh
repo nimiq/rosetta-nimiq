55bd7d722d23daf3b1524d6c1f316a71daeb4d33
Sören <hello@soerenschwert.de>
2022-01-07 09:30:13 +0100

    Add script to compile binaries for all supported platforms

---
+++ compile.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/sh
+
+rm dist/*
+
+deno compile --target x86_64-unknown-linux-gnu  --output dist/rosetta-linux         --allow-net --allow-read src/mod.ts
+deno compile --target x86_64-pc-windows-msvc    --output dist/rosetta-windows       --allow-net --allow-read src/mod.ts
+deno compile --target x86_64-apple-darwin       --output dist/rosetta-apple-x86     --allow-net --allow-read src/mod.ts
+deno compile --target aarch64-apple-darwin      --output dist/rosetta-apple-aarch   --allow-net --allow-read src/mod.ts
