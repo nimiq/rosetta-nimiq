e2c5bd22eed95e036135c9966fb194c248e11091
Sören <hello@soerenschwert.de>
2022-08-31 07:51:18 +0200

    Upgrade oak server to latest version for compatibility with new Deno version

--- src/deps/oak.ts
+++ src/deps/oak.ts
@@ -1,2 +1,2 @@
-export { Application, Router } from "https://deno.land/x/oak@v10.1.0/mod.ts";
-export type { Middleware } from "https://deno.land/x/oak@v10.1.0/middleware.ts";
+export { Application, Router } from "https://deno.land/x/oak@v11.1.0/mod.ts";
+export type { Middleware } from "https://deno.land/x/oak@v11.1.0/middleware.ts";
